124e6879121c924467ce8ba7545b4b74b652ea06
joer9514 <joer9514@gmail.com>
2021-07-19 13:36:58 -0500

    [Add] The task has been added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that return sum"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that return sum
+    args:
+        a: float number
+        b: float number
+    Return: sum
+    """
+    return a + b
