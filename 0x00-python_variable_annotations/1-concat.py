c716bcab15843bdb374ab86c8647f59780a2e5b0
joer9514 <joer9514@gmail.com>
2021-07-19 13:39:37 -0500

    [Add] The task 2 has been added

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The function that concat two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concant two strings"""
+    return str1 + "" + str2
