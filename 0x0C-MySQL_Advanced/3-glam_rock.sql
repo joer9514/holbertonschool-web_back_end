a101c3f0e41c67584745509fe10729bd24530c03
joer9514 <joer9514@gmail.com>
2021-09-01 22:52:13 -0500

    [ADD] - task 3 has been added

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- Script that lists all bands
+-- Ranked by their longevity
+SELECT
+    band_name,
+    COALESCE(split, 2020) - formed as lifespan
+FROM
+   metal_bands
+WHERE
+    style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
