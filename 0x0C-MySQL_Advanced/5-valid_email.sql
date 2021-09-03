f2c5a70cec983e2c5085fedb54bc9372faa796d7
joer9514 <joer9514@gmail.com>
2021-09-02 23:15:26 -0500

    [ADD] - Task 5 has been added

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a trigger that resets the attributes.
+CREATE TRIGGER new_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+	SET NEW.valid_email = 0;
+    END IF;
+END$$
